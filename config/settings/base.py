67271dee4883062fb963b2ac22b980e8ecbaf47e
Abe <abe@habet.dev>
2023-06-04 23:27:06 -0400

    add static folder

--- config/settings/base.py
+++ config/settings/base.py
@@ -141,7 +141,7 @@ MIDDLEWARE = [
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATIC_ROOT = "/var/www/myHebrewDates/staticfiles"
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
