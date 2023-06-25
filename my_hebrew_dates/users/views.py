ab4da0181d679484d2f841ffe1652017ed584448
Abe <abe@habet.dev>
2023-06-25 01:49:42 -0400

    redirect to calendar

--- my_hebrew_dates/users/views.py
+++ my_hebrew_dates/users/views.py
@@ -37,7 +37,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("hebcal:calendar_list")
 
 
 user_redirect_view = UserRedirectView.as_view()
