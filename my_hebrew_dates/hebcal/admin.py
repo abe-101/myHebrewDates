04a4cd699b7f65248d1ad110c84c466ca2d0527d
Abe <abe@habet.dev>
2023-06-30 19:45:19 -0400

    show the uuid in admin pannel

--- my_hebrew_dates/hebcal/admin.py
+++ my_hebrew_dates/hebcal/admin.py
@@ -12,6 +12,11 @@ class CalendarAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
     ]
+    list_display = ("name", "display_uuid")  # Include the display_uuid method in list_display
+
+    @admin.display(description="UUID")
+    def display_uuid(self, obj):
+        return obj.uuid  # Display the UUID field value
 
 
 admin.site.register(HebrewDate)
