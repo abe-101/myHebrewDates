fa0d0258751af24729cba7700817247823ea90b8
Abe Hanoka <abe@habet.dev>
2025-01-04 19:41:56 -0500

    more updates

--- config/settings/test.py
+++ config/settings/test.py
@@ -33,6 +33,6 @@ TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore[index]
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
-MEDIA_URL = "http://media.testserver"
+MEDIA_URL = "http://media.testserver/"
 # Your stuff...
 # ------------------------------------------------------------------------------
