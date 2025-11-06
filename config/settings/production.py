6ba836f95e394b03df171e61ffc7ece6a8019ac2
Abe Hanoka <abe@habet.dev>
2025-11-05 22:04:01 -0500

    add posthog

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,6 +10,7 @@ from sentry_sdk.integrations.redis import RedisIntegration
 from .base import *  # noqa: F403
 from .base import DATABASES
 from .base import INSTALLED_APPS
+from .base import MIDDLEWARE
 from .base import REDIS_URL
 from .base import SPECTACULAR_SETTINGS
 from .base import env
@@ -238,3 +239,27 @@ SPECTACULAR_SETTINGS["SERVERS"] = [
 ]
 # Your stuff...
 # ------------------------------------------------------------------------------
+# PostHog
+# ------------------------------------------------------------------------------
+# Add PostHog middleware only in production
+MIDDLEWARE += ["posthog.integrations.django.PosthogContextMiddleware"]
+
+POSTHOG_API_KEY = env("POSTHOG_API_KEY", default="")
+
+
+def add_request_context(request):
+    # type: (HttpRequest) -> Dict[str, Any]
+    tags = {}
+    if hasattr(request, "user") and request.user.is_authenticated:
+        tags["user_type"] = "authenticated"
+        tags["user_id"] = str(request.user.id)
+    else:
+        tags["user_type"] = "anonymous"
+
+    # Add request info
+    tags["user_agent"] = request.headers.get("user-agent", "")
+    return tags
+
+
+POSTHOG_MW_EXTRA_TAGS = add_request_context
+POSTHOG_MW_CAPTURE_EXCEPTIONS = True
--- my_hebrew_dates/core/apps.py
+++ my_hebrew_dates/core/apps.py
@@ -1,6 +1,15 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "my_hebrew_dates.core"
+
+    def ready(self):
+        if os.environ.get("DJANGO_SETTINGS_MODULE") == "config.settings.production":
+            import posthog
+
+            posthog.api_key = settings.POSTHOG_API_KEY
+            posthog.host = "https://m.myhebrewdates.com"
